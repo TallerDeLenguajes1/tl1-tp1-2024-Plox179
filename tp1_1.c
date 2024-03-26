afb58515bed08811870419fa51ab7bff04c56583
“Plox179” <leandroleon799@gmail.com>
2024-03-26 16:48:17 -0300

    añado la parte de punteros

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hola mundo");
+    printf("Hola mundo\n");
+
+    int numero=79;
+    int *puntero=&numero;
+
+    printf("%d\n", *puntero); // muestro el contenido del puntero
+    printf("%p\n", *puntero); // muestro la dirección de memoria almacenada por el puntero
+    printf("%p\n", &numero); // muestro la dirección de memoria de la variable numero
+    printf("%p\n", &puntero); // muestro la dirección de memoria del puntero
+    printf("%d\n", sizeof(numero));
+
     return 0;
 }
